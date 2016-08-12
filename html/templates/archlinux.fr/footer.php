e0192a26d2eab1df3fca058ad16468090099cefa
Skunnyk <skunnyk@alteroot.org>
2016-08-12 20:05:05 +0200

    Update copyright

--- html/templates/archlinux.fr/footer.php
+++ html/templates/archlinux.fr/footer.php
@@ -1,12 +1,7 @@
 <div id="footer">
-	<p>Design depuis <a href="http://www.archlinux.org" title="Design d' Archlinux.org">Archlinux.org</a>
-	&#183;
-	<a href="http://validator.w3.org/check/referer" title="Denne side valideres med XHTML 1.1">XHTML</a>
-	&#183;
-	<a href="http://jigsaw.w3.org/css-validator/check/referer" title="Denne side valideres med CSS">CSS</a>
-	</p>
+	<p>Design depuis <a href="https://www.archlinux.org" title="Design d' Archlinux.org">Archlinux.org</a>
 	<p>Propulsé par Afur</p>
-	<p>© 2010 Archlinux.fr ~ Communauté Francophone Arch Linux</p>
+	<p>© 2016 Archlinux.fr ~ Communauté Francophone Arch Linux</p>
 </div>
 </div>
 </body>
